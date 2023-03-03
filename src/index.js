edf8725bc692649a74ae3a9a8f4809c549513328
Abeera Malik <abeeramalik108@gmail.com>
2023-03-03 17:45:00 +0500

    initial commit
    commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from"react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+
+  <>
+      <h1>           ABEERA's Netfllix Pick      </h1>
+      <p> Here the list of my fav 5 netflix series </p>
+       <ol>
+           <li> DARK </li>
+             <li> Wednesday </li>
+              <li> StrangerThings </li>
+             <li> MR Robot</li>
+               <li> Extra curricular </li>
+          </ol>
+           </>,
+         document.getElementById("root")
+  );
